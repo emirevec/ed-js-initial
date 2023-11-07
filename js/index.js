0a2818a2ebabf1efbbc53b986b0f014f80eec25e
emirevec <vecchioemiliano@gmail.com>
2023-11-07 12:13:33 -0300

    feat(web) add button to start script

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-
+let d = document;
+let energía = prompt("Ingrese energía total generada, y presione aceptar.");
+let presupuesto = prompt("Ingrese el presupuesto disponible, y presione aceptar.");
+let tiempo = prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
+let efectividad = energía/(presupuesto * tiempo);
+alert(efectividad);
