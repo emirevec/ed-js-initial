d38a4da255b9a419c6ab980aa30ac9c56217e56e
emirevec <vecchioemiliano@gmail.com>
2023-11-08 10:39:49 -0300

    feat(web) add script to button 'calcular'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
-let d = document;
-let energía = prompt("Ingrese energía total generada, y presione aceptar.");
-let presupuesto = prompt("Ingrese el presupuesto disponible, y presione aceptar.");
-let tiempo = prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
-let efectividad = energía/(presupuesto * tiempo);
-alert(efectividad);
+const d = document;
+const btnCalcluar = d.querySelector("button")
+let energia;  
+let presupuesto; 
+let tiempo; 
+
+
+function calcular(){
+    energia = prompt("Ingrese energía total generada, y presione aceptar.");
+    presupuesto = prompt("Ingrese el presupuesto disponible, y presione aceptar.");
+    tiempo =prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
+    const efectividad = energia / (presupuesto * tiempo);
+    alert(efectividad)
+}
+
+btnCalcluar.addEventListener("click", calcular);
+
+//prompt("Ingrese el presupuesto disponible, y presione aceptar.");
+//prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
+
+/* function start (){
+    ingresarEnergia();
+} */
+
+/* function ingresarEnergia (){
+    energia = parseInt(prompt("Ingrese energía total generada, y presione aceptar."));
+    if (Number.isNaN(energia)){
+        alert("Error");
+    }
+    console.log(energia);
+} */
