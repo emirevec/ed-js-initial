686ba5f44a3d0f5564595e91cb37f1b5746203d4
emirevec <vecchioemiliano@gmail.com>
2023-11-08 11:11:49 -0300

    refactor(web) improve 'calcular' buttom script

--- js/index.js
+++ js/index.js
@@ -5,27 +5,35 @@ let presupuesto;
 let tiempo; 
 
 
-function calcular(){
+function calcuar (){
+    energia = parseInt(prompt("Ingrese energía total generada, y presione aceptar."));
+    if (Number.isNaN(energia)){
+        alert("Error! Debe ingresar un número entero");
+        return;
+    }
+    
+    presupuesto = parseInt(prompt("Ingrese el presupuesto disponible, y presione aceptar."));
+    if (Number.isNaN(presupuesto)){
+        alert("Error! Debe ingresar un número entero");
+        return;
+    }
+    
+    tiempo = parseInt(prompt("Ingrese energía el tiempo utilizado, y presione aceptar."));
+    if (Number.isNaN(tiempo)){
+        alert("Error! Debe ingresar un número entero");
+        return;
+    }
+    const efectividad = energia / (presupuesto * tiempo);
+    alert(efectividad)
+}
+
+/* function calcular(){
     energia = prompt("Ingrese energía total generada, y presione aceptar.");
     presupuesto = prompt("Ingrese el presupuesto disponible, y presione aceptar.");
     tiempo =prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
     const efectividad = energia / (presupuesto * tiempo);
     alert(efectividad)
-}
-
-btnCalcluar.addEventListener("click", calcular);
-
-//prompt("Ingrese el presupuesto disponible, y presione aceptar.");
-//prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
-
-/* function start (){
-    ingresarEnergia();
 } */
 
-/* function ingresarEnergia (){
-    energia = parseInt(prompt("Ingrese energía total generada, y presione aceptar."));
-    if (Number.isNaN(energia)){
-        alert("Error");
-    }
-    console.log(energia);
-} */
+btnCalcluar.addEventListener("click", calcuar);
+//btnCalcluar.addEventListener("click", calcular);
