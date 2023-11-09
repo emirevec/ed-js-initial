da47434d0de4347ab2638ec4ff599c34044be201
emirevec <vecchioemiliano@gmail.com>
2023-11-09 10:44:37 -0300

    refactor(web) impruve 'calcular' function

--- js/index.js
+++ js/index.js
@@ -4,36 +4,33 @@ let energia;
 let presupuesto; 
 let tiempo; 
 
+function validarDatos(dato){
+    if (Number.isNaN(dato)){
+        alert("Error! Debe ingresar un número entero");
+        return null;
+    }
+    return dato;
+}
 
 function calcuar (){
-    energia = parseInt(prompt("Ingrese energía total generada, y presione aceptar."));
-    if (Number.isNaN(energia)){
-        alert("Error! Debe ingresar un número entero");
+    energia = validarDatos(parseInt(prompt("Ingrese energía total generada, y presione aceptar.")));
+    if (energia === null){
         return;
     }
     
-    presupuesto = parseInt(prompt("Ingrese el presupuesto disponible, y presione aceptar."));
-    if (Number.isNaN(presupuesto)){
-        alert("Error! Debe ingresar un número entero");
+    presupuesto = validarDatos(parseInt(prompt("Ingrese energía total generada, y presione aceptar.")));
+    if (presupuesto === null){
         return;
     }
     
-    tiempo = parseInt(prompt("Ingrese energía el tiempo utilizado, y presione aceptar."));
-    if (Number.isNaN(tiempo)){
-        alert("Error! Debe ingresar un número entero");
+    tiempo = validarDatos(parseInt(prompt("Ingrese energía total generada, y presione aceptar.")));
+    if (tiempo === null){
         return;
     }
+    
     const efectividad = energia / (presupuesto * tiempo);
     alert(efectividad)
 }
 
-/* function calcular(){
-    energia = prompt("Ingrese energía total generada, y presione aceptar.");
-    presupuesto = prompt("Ingrese el presupuesto disponible, y presione aceptar.");
-    tiempo =prompt("Ingrese energía el tiempo utilizado, y presione aceptar.");
-    const efectividad = energia / (presupuesto * tiempo);
-    alert(efectividad)
-} */
-
 btnCalcluar.addEventListener("click", calcuar);
-//btnCalcluar.addEventListener("click", calcular);
+
