545e37833385986e3a60b35da6be8a3259b68d88
emirevec <vecchioemiliano@gmail.com>
2023-11-16 10:12:53 -0300

    build(web) add fil [data]

---
+++ js/data.js
@@ -0,0 +1,18 @@
+let data = {
+    info: [
+        {
+            tipoDeEnergia:"Eólica",
+            descripcion:"Molino a 2km",
+            energiaGenerada:"1500",
+            presupuesto:"500000",
+            tiempoEstimado:"2"
+        },
+        {
+            tipoDeEnergia:"Eólica",
+            descripcion:"Molino arriba de la planta",
+            energiaGenerada:"1300",
+            presupuesto:"650000",
+            tiempoEstimado:"1"
+        }
+    ]
+}
