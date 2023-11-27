360abd9330db05ac259278dd3a8094bcffb9834d
emirevec <vecchioemiliano@gmail.com>
2023-11-27 11:31:32 -0300

    feat(web) add template handler to portrait in onclick function on form submit

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import tiposEnergia from "./tiposEnergia.js";
 
 let data = {info: []};
 let template = Handlebars.templates["table"];
+let templatePortrait = Handlebars.templates["tablePortrait"];
 
 function start (){
     const selectImplementarTipos = $("#implementar_tipos");
@@ -30,6 +31,7 @@ $("#implementar_submit").on("click", function(e){
 
     data.info.push(nuevaImplementacion);
     $("#table").append(template(data));
+    $("#tablePortrait").append(templatePortrait(data));
     data = {info: []};
 });
 
