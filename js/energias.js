fbc91e878554414201dc96da6b865ee93d5e4dce
emirevec <vecchioemiliano@gmail.com>
2023-11-28 12:26:54 -0300

    perf(web) add file [energias.js]

---
+++ js/energias.js
@@ -0,0 +1,18 @@
+let energias = [
+    {
+        nombre: "Eólica",
+        eficiencia: 0
+    },
+    {
+        nombre: "Hidroeléctrica",
+        eficiencia: 0
+    },
+    {
+        nombre: "Solar",
+        eficiencia: 0
+    }
+]
+
+console.log(energias[0].nombre);
+
+export default energias;
