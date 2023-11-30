9918df8a8237ed9b6706184627c4fb25feb845a7
emirevec <vecchioemiliano@gmail.com>
2023-11-30 11:07:21 -0300

    fix(web) add 'implementaciones' to energia

--- js/energias.js
+++ js/energias.js
@@ -2,15 +2,18 @@ let energias = {
     energia:[
     {
         nombre: "Eólica",
-        eficiencia: 0
+        eficiencia: 0,
+        implementaciones: 0
     },
     {
         nombre: "Hidroeléctrica",
-        eficiencia: 0
+        eficiencia: 0,
+        implementaciones: 0
     },
     {
         nombre: "Solar",
-        eficiencia: 0
+        eficiencia: 0,
+        implementaciones: 0
     }
 ]}
 
